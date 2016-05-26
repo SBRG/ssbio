5c4590199495f2ee2c3ed50f0c31765e217879a8
Nathan Mih <nmih@ucsd.edu>
2016-05-26 15:21:55 -0700

    move around readme

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     packages=find_packages(),
     package_dir={'ssbio': 'ssbio'},
     package_data={'ssbio': ['ssbio/etc/*']},
-    scripts = ['ssbio/structure/cleanpdb', 'ssbio/structure/mutatepdb.py', 'ssbio/structure/tleap.py',
+    scripts = ['ssbio/scripts/cleanpdb', 'ssbio/structure/mutatepdb.py', 'ssbio/structure/tleap.py',
                'ssbio/databases/drugbank.py', 'ssbio/structure/properties/msmsprops.py',
                'ssbio/dock/dock.py'],
     long_description=open('README.md').read(),
