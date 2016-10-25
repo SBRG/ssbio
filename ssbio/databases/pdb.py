eeac1a60f9916d54c9a130ec34c1adbe15459b80
nmih <nmih@ucsd.edu>
2016-10-25 16:58:10 -0700

    Update behavior of saving and loading web requests for blasting pdbs and finding best structures

--- ssbio/databases/pdb.py
+++ ssbio/databases/pdb.py
@@ -1,16 +1,22 @@
 # TODO: 160501 - copied from ssbio_new - please ORGANIZE!
 
-import gzip
+import json
 import io
-from collections import defaultdict
-from io import BytesIO
-
-import cachetools
-import pandas as pd
 import requests
-import xmltodict
+import pandas as pd
+import os.path as op
+from io import BytesIO
+import cachetools
+import gzip
 
+from Bio.PDB import PDBList
+from lxml import etree
 from ssbio import utils
+from ssbio.structure.cleanpdb import CleanPDB
+from ssbio.structure.pdbioext import PDBIOExt
+
+import logging
+log = logging.getLogger(__name__)
 
 try:
     import urllib.request as urllib2
@@ -23,10 +29,6 @@ except ImportError:
 
 SEVEN_DAYS = 60 * 60 * 24 * 7
 
-from collections import defaultdict
-from Bio.PDB import PDBList
-from ssbio.structure.pdbioext import PDBIOExt
-from ssbio.structure.cleanpdb import CleanPDB
 
 def download_and_load_pdb(pdb_id, output_dir, file_type='pdb'):
     """Download a PDB ID to a specified output directory, and of a specified file format (currently only .pdb supported)
@@ -43,6 +45,7 @@ def download_and_load_pdb(pdb_id, output_dir, file_type='pdb'):
     # TODO: currently saves as .ent file
     file_loc = pdbl.retrieve_pdb_file(pdb_id, pdir=output_dir)
     return file_loc
+
 
 def download_and_clean_pdb(pdb_id, chain_id, output_dir, file_type='pdb', output_name=''):
     """Download the original PDB ID as well as a "cleaned" version of it including only the chain of interest.
@@ -76,10 +79,6 @@ def download_and_clean_pdb(pdb_id, chain_id, output_dir, file_type='pdb', output
     return my_clean_pdb
 
 
-from lxml import etree
-from io import StringIO, BytesIO
-
-
 def map_uniprot_resnum_to_pdb(uniprot_resnum, chain_id, sifts_file):
     """Map a UniProt residue number to its corresponding PDB residue number.
 
@@ -95,7 +94,7 @@ def map_uniprot_resnum_to_pdb(uniprot_resnum, chain_id, sifts_file):
         (mapped_resnum, is_observed) tuple - is_observed indicates if the 3D structure actually shows the residue
 
     """
-    # load the xml with lxml
+    # Load the xml with lxml
     parser = etree.XMLParser(ns_clean=True)
     tree = etree.parse(sifts_file, parser)
     root = tree.getroot()
@@ -105,26 +104,27 @@ def map_uniprot_resnum_to_pdb(uniprot_resnum, chain_id, sifts_file):
     # TODO: "Engineered_Mutation is also a possible annotation, need to figure out what to do with that
     my_pdb_annotation = False
 
-    # find the right chain (entities in the xml doc)
-    for chain in root.findall('.//{http://www.ebi.ac.uk/pdbe/docs/sifts/eFamily.xsd}entity'):
+    # Find the right chain (entities in the xml doc)
+    ent = './/{http://www.ebi.ac.uk/pdbe/docs/sifts/eFamily.xsd}entity'
+    for chain in root.findall(ent):
         if chain.attrib['entityId'] == chain_id:
-            # need to find the "crossRefDb" tag that has the attributes dbSource="UniProt" and  dbResNum="your_resnum_here"
-            # then match it to the crossRefDb dbResNum that has the attribute dbSource="PDBresnum"
+            # Find the "crossRefDb" tag that has the attributes dbSource="UniProt" and  dbResNum="your_resnum_here"
+            # Then match it to the crossRefDb dbResNum that has the attribute dbSource="PDBresnum"
 
-            # check if uniprot + resnum even exists in the sifts file (it won't if the pdb doesn't contain the residue)
-            my_uniprot_residue = chain.findall(
-                './/{http://www.ebi.ac.uk/pdbe/docs/sifts/eFamily.xsd}crossRefDb[@dbSource="UniProt"][@dbResNum="%s"]' % uniprot_resnum)
+            # Check if uniprot + resnum even exists in the sifts file (it won't if the pdb doesn't contain the residue)
+            ures = './/{http://www.ebi.ac.uk/pdbe/docs/sifts/eFamily.xsd}crossRefDb[@dbSource="UniProt"][@dbResNum="%s"]' % uniprot_resnum
+            my_uniprot_residue = chain.findall(ures)
             if len(my_uniprot_residue) == 1:
-                # get crossRefDb dbSource="PDB"
+                # Get crossRefDb dbSource="PDB"
                 parent = my_uniprot_residue[0].getparent()
-                my_pdb_residue = parent.findall(
-                    './/{http://www.ebi.ac.uk/pdbe/docs/sifts/eFamily.xsd}crossRefDb[@dbSource="PDB"]')
+                pres = './/{http://www.ebi.ac.uk/pdbe/docs/sifts/eFamily.xsd}crossRefDb[@dbSource="PDB"]'
+                my_pdb_residue = parent.findall(pres)
                 my_pdb_resnum = int(my_pdb_residue[0].attrib['dbResNum'])
 
-                # get <residueDetail dbSource="PDBe" property="Annotation">
-                # will be Not_Observed if it is not seen in the PDB
-                my_pdb_annotation = parent.findall(
-                    './/{http://www.ebi.ac.uk/pdbe/docs/sifts/eFamily.xsd}residueDetail[@dbSource="PDBe"][@property="Annotation"]')
+                # Get <residueDetail dbSource="PDBe" property="Annotation">
+                # Will be Not_Observed if it is not seen in the PDB
+                anno = './/{http://www.ebi.ac.uk/pdbe/docs/sifts/eFamily.xsd}residueDetail[@dbSource="PDBe"][@property="Annotation"]'
+                my_pdb_annotation = parent.findall(anno)
                 if len(my_pdb_annotation) == 1:
                     my_pdb_annotation = my_pdb_annotation[0].text
                     if my_pdb_annotation == 'Not_Observed':
@@ -132,9 +132,9 @@ def map_uniprot_resnum_to_pdb(uniprot_resnum, chain_id, sifts_file):
                 else:
                     my_pdb_annotation = True
             else:
-                return (None, False)
+                return None, False
 
-    return (my_pdb_resnum, my_pdb_annotation)
+    return my_pdb_resnum, my_pdb_annotation
 
 
 @cachetools.func.ttl_cache(maxsize=1, ttl=SEVEN_DAYS)
@@ -146,7 +146,7 @@ def _theoretical_pdbs():
     Returns: list of theoretical PDBs
 
     """
-    theoretical_pdbs_link = "ftp://ftp.wwpdb.org/pub/pdb/data/structures/models/index/titles.idx"
+    theoretical_pdbs_link = "ftp://ftp.wwpdb.org/pub/pdb/data_dir/structures/models/index/titles.idx"
     response = urllib2.urlopen(theoretical_pdbs_link)
     theoretical_pdbs_raw = BytesIO(response.read())
     theoretical_pdbs_df = pd.read_table(theoretical_pdbs_raw, sep='\t', header=None)
@@ -188,7 +188,7 @@ def _obsolete_pdb_mapping():
     """
     pdb_obsolete_mapping = {}
 
-    req = urllib2.Request('ftp://ftp.wwpdb.org/pub/pdb/data/status/obsolete.dat')
+    req = urllib2.Request('ftp://ftp.wwpdb.org/pub/pdb/data_dir/status/obsolete.dat')
     response = urllib2.urlopen(req)
     output = response.read().decode('utf-8').strip().split('\n')
     for line in output:
@@ -225,7 +225,8 @@ def get_replacement_pdbs(pdb_id):
     Args:
         pdb_id (str): PDB ID
 
-    Returns: a list of replacement PDB IDs,
+    Returns:
+        A list of replacement PDB IDs,
         None if the PDB is not obsolete or there is no replacement
 
     """
@@ -240,7 +241,13 @@ def get_replacement_pdbs(pdb_id):
 
 def pdb_current_checker(pdb_ids):
     """Batch check if a list of PDBs is current, theoretical, obsolete. If obsolete, provide new mapping.
-    # status can be "theoretical", "current", or a PDB ID giving the non-obsolete entry
+
+    Args:
+        pdb_ids (list): List of PDB IDs
+
+    Returns:
+        dict: Dictionary of {pdb_id: <status>}, where status can be
+            "theoretical", "current", or a list of PDB IDs giving the non-obsolete entry
     """
     pdb_ids = utils.force_list(pdb_ids)
     pdb_ids = [x.lower() for x in pdb_ids]
@@ -259,6 +266,162 @@ def pdb_current_checker(pdb_ids):
         pdb_status[c] = 'current'
 
     return pdb_status
+
+
+@cachetools.func.ttl_cache(maxsize=256)
+def best_structures(uniprot_id, outfile='', outdir='', force_rerun=False):
+    """Utilize the PDBe REST API to return the rank-ordered list of PDB "best structures".
+
+    The URL to access the results is formatted like so:
+    https://www.ebi.ac.uk/pdbe/api/mappings/best_structures/:accession
+
+    Args:
+        uniprot_id (str): UniProt Accession ID
+
+    Returns:
+        list: Rank-ordered list of dictionaries representing chain-specific PDB entries
+
+    """
+    outfile = op.join(outdir, outfile)
+
+    # Load the existing json file
+    if outfile and op.exists(outfile) and not force_rerun:
+        with open(outfile, 'r') as f:
+            data = json.load(f)
+
+    # Otherwise run the web request
+    else:
+        response = requests.get('https://www.ebi.ac.uk/pdbe/api/mappings/best_structures/{}'.format(uniprot_id),
+                                data={'key': 'value'})
+
+        if response.status_code == 404:
+            log.error('404 returned for {}, probably no structures available.'.format(uniprot_id))
+            return []
+
+        data = dict(response.json())[uniprot_id]
+
+        # Write the json file if specified
+        if outfile:
+            with open(outfile, 'w') as f:
+                json.dump(data, f)
+
+    return data
+
+
+@cachetools.func.ttl_cache(maxsize=256)
+def blast_pdb(seq, outfile='', outdir='', force_rerun=False, evalue=0.0001, link=False):
+    """Returns a list of BLAST hits of a sequence to available structures in the PDB.
+
+    Args:
+        seq (str): Your sequence, in string format
+        outfile (str): Name of output file
+        outdir (str, optional): Path to output directory. Default is the current directory.
+        force_rerun (bool, optional): If existing BLAST results should not be used, set to True. Default is False
+        evalue (float, optional): Cutoff for the E-value - filters for significant hits. 0.001 is liberal, 0.0001 is stringent (default).
+        link (bool, optional): Set to True if a link to the HTML results should be displated
+
+    Returns:
+        list: Rank ordered list of BLAST hits in dictionaries.
+
+    """
+
+    if len(seq) < 12:
+        raise ValueError('Sequence must be at least 12 residues long.')
+    if link:
+        page = 'PDB results page: http://www.rcsb.org/pdb/rest/getBlastPDB1?sequence={}&eCutOff={}&maskLowComplexity=yes&matrix=BLOSUM62&outputFormat=HTML'.format(seq, evalue)
+        print(page)
+
+    parser = etree.XMLParser(ns_clean=True)
+
+    outfile = op.join(outdir, outfile)
+    if outfile and op.exists(outfile) and not force_rerun:
+        # Parse the existing XML file
+        tree = etree.parse(outfile, parser)
+    else:
+        # Load the BLAST XML results if force_rerun=True
+        page = 'http://www.rcsb.org/pdb/rest/getBlastPDB1?sequence={}&eCutOff={}&maskLowComplexity=yes&matrix=BLOSUM62&outputFormat=XML'.format(
+                        seq, evalue)
+        req = requests.get(page)
+        if req.status_code == 200:
+            response = req.text
+
+            # Save the XML file
+            if outfile:
+                with open(outfile, 'w') as f:
+                    f.write(response)
+
+            # Parse the XML string
+            tree = etree.ElementTree(etree.fromstring(response, parser))
+
+        else:
+            log.error('BLAST request timed out')
+            return []
+
+    # Get length of original sequence to calculate percentages
+    len_orig = float(len(seq))
+
+    root = tree.getroot()
+    hit_list = []
+
+    for hit in root.findall('BlastOutput_iterations/Iteration/Iteration_hits/Hit'):
+        info = {}
+
+        hitdef = hit.find('Hit_def')
+        if hitdef is not None:
+            info['hit_pdb'] = hitdef.text.split('|')[0].split(':')[0]
+            info['hit_pdb_chains'] = hitdef.text.split('|')[0].split(':')[2].split(',')
+
+        # One PDB can align to different parts of the sequence
+        # Will just choose the top hit for this single PDB
+        hsp = hit.findall('Hit_hsps/Hsp')[0]
+
+        # Number of identical residues
+        hspi = hsp.find('Hsp_identity')
+        if hspi is not None:
+            info['hit_num_ident'] = int(hspi.text)
+            info['hit_percent_ident'] = int(hspi.text)/len_orig
+
+        # Number of similar residues (positive hits)
+        hspp = hsp.find('Hsp_positive')
+        if hspp is not None:
+            info['hit_num_similar'] = int(hspp.text)
+            info['hit_percent_similar'] = int(hspp.text) / len_orig
+
+        # Total number of gaps (unable to align in either query or subject)
+        hspg = hsp.find('Hsp_gaps')
+        if hspg is not None:
+            info['hit_num_gaps'] = int(hspg.text)
+            info['hit_percent_gaps'] = int(hspg.text) / len_orig
+
+        # E-value of BLAST
+        hspe = hsp.find('Hsp_evalue')
+        if hspe is not None:
+            info['hit_evalue'] = float(hspe.text)
+
+        # Score of BLAST
+        hsps = hsp.find('Hsp_score')
+        if hsps is not None:
+            info['hit_score'] = float(hsps.text)
+
+        hit_list.append(info)
+
+    log.debug("Number of BLAST hits: {}".format(len(hit_list)))
+    return hit_list
+
+
+def blast_pdb_df(seq, xml_outfile, xml_outdir='', force_rerun=False, evalue=0.001, link=False):
+    """Make a dataframe of BLAST results
+
+    Args: see blast_pdb
+
+    Returns:
+        Tuple of (original results, parsed results in Pandas DataFrame)
+
+    """
+    blast_results = blast_pdb(seq=seq, outfile=xml_outfile, outdir=xml_outdir, force_rerun=force_rerun, evalue=evalue, link=link)
+    cols = ['hit_pdb', 'hit_pdb_chains', 'hit_evalue', 'hit_score', 'hit_num_ident', 'hit_percent_ident',
+            'hit_num_similar', 'hit_percent_similar', 'hit_num_gaps', 'hit_percent_gaps']
+    return blast_results, pd.DataFrame.from_records(blast_results, columns=cols)
 
 
 @cachetools.func.ttl_cache(maxsize=1, ttl=SEVEN_DAYS)
@@ -287,96 +450,6 @@ def SIFTS():
 
 def sifts_pdb_chain_to_uniprot(pdb, chain):
     return _sifts_mapping().ix[(pdb.lower(), chain.upper())]['SP_PRIMARY'].unique().tolist()
-
-
-@cachetools.func.ttl_cache(maxsize=128)
-def blast_pdb(seq, evalue=0.001, link=False):
-    """
-    Returns a DataFrame of BLAST hits to available structures in the PDB
-
-    :param string seq: your sequence, in string format
-    :param float evalue: cutoff for the E-value - filters for significant hits. 0.001 is liberal, 0.0001 is stringent
-    :return Pandas DataFrame:
-    """
-
-    if len(seq) < 12:
-        print('ERROR: Sequence must be at least 12 residues long')
-        return None
-    if link:
-        print('Click here for the PDB results page: http://www.rcsb.org/pdb/rest/getBlastPDB1?sequence={}&eCutOff={}&maskLowComplexity=yes&matrix=BLOSUM62&outputFormat=HTML'.format(seq, evalue))
-    req = urllib2.Request(
-        'http://www.rcsb.org/pdb/rest/getBlastPDB1?sequence={}&eCutOff={}&maskLowComplexity=yes&matrix=BLOSUM62&outputFormat=XML'.format(
-            seq, evalue))
-    response = urllib2.urlopen(req)
-
-    if response.getcode() == 200:
-        info_dict = defaultdict(dict)
-        len_orig = len(seq)
-
-        raw = BytesIO(response.read())
-        doc = xmltodict.parse(raw)
-        if 'Iteration_hits' not in doc['BlastOutput']['BlastOutput_iterations']['Iteration'].keys():
-            # print('ERROR: No hits found')
-            return pd.DataFrame()
-
-        else:
-            hit_list = utils.force_list(
-                doc['BlastOutput']['BlastOutput_iterations']['Iteration']['Iteration_hits']['Hit'])
-            for i, hit in enumerate(hit_list):
-                hit_pdb = hit['Hit_def'].split('|')[0].split(':')[0]
-                hit_pdb_chain = hit['Hit_def'].split('|')[0].split(':')[2]
-
-                hit_dict = hit['Hit_hsps']['Hsp']
-                if isinstance(hit_dict, list):
-                    hit_dict = hit_dict[0]
-
-                info_dict[i]['hit_pdb'] = hit_pdb
-                info_dict[i]['hit_pdb_chain'] = hit_pdb_chain
-                info_dict[i]['hit_seq'] = hit_dict['Hsp_hseq']
-                info_dict[i]['hit_num_ident'] = hit_dict['Hsp_identity']
-                info_dict[i]['hit_percent_ident'] = float(hit_dict['Hsp_identity']) / float(len_orig)
-                info_dict[i]['hit_evalue'] = hit_dict['Hsp_evalue']
-                info_dict[i]['hit_score'] = hit_dict['Hsp_score']
-
-                # TODO: what about sequence similarity?
-
-            info_df = pd.DataFrame.from_dict(info_dict, orient='index')
-            reorg = ['hit_pdb', 'hit_pdb_chain', 'hit_evalue', 'hit_score', 'hit_num_ident', 'hit_percent_ident']
-            # print("Number of BLAST hits: {}".format(len(info_df)))
-            return info_df[reorg]
-
-    else:
-        # print('ERROR: BLAST request timed out')
-        return pd.DataFrame()
-
-
-def top_pdb_blast_hit(seq, evalue=0.001):
-    blast_df = blast_pdb(seq, evalue)
-    if blast_df.empty:
-        return None
-    return blast_df.loc[0].to_dict()
-
-@cachetools.func.ttl_cache(maxsize=500)
-def best_structures(uniprot_id):
-    """Utilize the PDBe REST API to return the rank-ordered list of PDB "best structures"
-
-    The URL to access the results is formatted like so:
-    https://www.ebi.ac.uk/pdbe/api/mappings/best_structures/:accession
-
-    Args:
-        uniprot_id (str): UniProt Accession ID
-
-    Returns:
-        Rank-ordered list of dictionaries representing chain-specific PDB entries
-
-    """
-
-    response = requests.get('https://www.ebi.ac.uk/pdbe/api/mappings/best_structures/{}'.format(uniprot_id),
-                            data={'key': 'value'})
-    if response.status_code == 404:
-        return {}
-    return dict(response.json())[uniprot_id]
-
 #
 #
 # class Structure():
