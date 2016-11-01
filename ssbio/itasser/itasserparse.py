acc03f3817b5e208d594efc628c39c2f2a4300ea
nmih <nmih@ucsd.edu>
2016-10-31 21:28:45 -0700

    Parse init.dat, cscore

--- ssbio/itasser/itasserparse.py
+++ ssbio/itasser/itasserparse.py
@@ -1,4 +1,123 @@
+import os.path as op
 import pandas as pd
+import shutil
+import os
+import logging
+log = logging.getLogger(__name__)
+import time
+
+def organize_itasser_models(raw_dir, copy_to_dir, rename_model_to=''):
+    """Reorganize the raw information from I-TASSER modeling.
+
+    - Copies the model1.pdb file and COACH results (if they exist) to specified folder
+    - Parses modeling information such as C-scores, template, etc
+    - Also will parse binding site (COACH) information if it exists
+
+    Returns:
+        dict: summary of I-TASSER results for a sequence
+    """
+
+    hom_dict = {}
+
+    # Best homology model is "model1.pdb"
+    model = 'model1.pdb'
+    old_model_path = op.join(raw_dir, model)
+    new_model_path = op.join(copy_to_dir, model)
+    if rename_model_to:
+        new_model_path = op.join(copy_to_dir, '{}.pdb'.format(rename_model_to))
+    if op.exists(old_model_path):
+        if not op.exists(new_model_path):
+            shutil.copy2(old_model_path, new_model_path)
+        hom_dict['model_file'] = op.basename(new_model_path)
+        hom_dict['model_date'] = time.strftime('%Y-%m-%d', time.gmtime(os.path.getmtime(old_model_path)))
+    else:
+        log.debug('{}: No homology model exists'.format(raw_dir))
+
+    ### Other modeling results
+    dest_itasser_dir = op.join(copy_to_dir, 'itasser')
+    if rename_model_to:
+        dest_itasser_dir = op.join(copy_to_dir, '{}_itasser'.format(rename_model_to))
+    if not op.exists(dest_itasser_dir) and op.exists(old_model_path):
+        os.mkdir(dest_itasser_dir)
+
+    # init.dat
+    init_dat = 'init.dat'
+    old_init_dat = op.join(raw_dir, init_dat)
+    new_init_dat = op.join(dest_itasser_dir, init_dat)
+    if op.exists(old_init_dat):
+        hom_dict.update(parse_init_dat(old_init_dat))
+        if not op.exists(new_init_dat):
+            shutil.copy2(old_init_dat, new_init_dat)
+
+    # seq.dat
+    seq_dat = 'seq.dat'
+    old_seq_dat = op.join(raw_dir, seq_dat)
+    new_seq_dat = op.join(dest_itasser_dir, seq_dat)
+    if op.exists(old_seq_dat):
+        if not op.exists(new_seq_dat):
+            shutil.copy2(old_seq_dat, new_seq_dat)
+            # TODO: parse seq.dat
+
+    # cscore
+    cscore = 'cscore'
+    old_cscore = op.join(raw_dir, cscore)
+    new_cscore = op.join(dest_itasser_dir, cscore)
+    if op.exists(old_cscore):
+        hom_dict.update(parse_cscore(old_cscore))
+        if not op.exists(new_cscore):
+            shutil.copy2(old_cscore, new_cscore)
+
+    ### COACH (binding site prediction) results
+    model1_coach_folder = op.join(raw_dir, 'model1', 'coach')
+    dest_coach_dir = op.join(dest_itasser_dir, 'coach')
+    if rename_model_to:
+        dest_coach_dir = op.join(dest_itasser_dir, '{}_coach'.format(rename_model_to))
+    if not op.exists(dest_coach_dir) and op.exists(model1_coach_folder):
+        os.mkdir(dest_coach_dir)
+
+    # Bsites.inf
+    bsites_inf = 'Bsites.inf'
+    old_bsites_inf = op.join(model1_coach_folder, bsites_inf)
+    new_bsites_inf = op.join(dest_coach_dir, bsites_inf)
+    if op.exists(old_bsites_inf):
+        if not op.exists(new_bsites_inf):
+            shutil.copy2(old_bsites_inf, new_bsites_inf)
+        # TODO: parse Bsites.inf
+
+    # EC.dat
+    ec = 'EC.dat'
+    old_ec = op.join(model1_coach_folder, ec)
+    new_ec = op.join(dest_coach_dir, ec)
+    if op.exists(old_ec):
+        if not op.exists(new_ec):
+            shutil.copy2(old_ec, new_ec)
+        # TODO: parse EC
+
+    go_mf = 'GO_MF.dat'
+    old_go_mf = op.join(model1_coach_folder, go_mf)
+    new_go_mf = op.join(dest_coach_dir, go_mf)
+    if op.exists(old_go_mf):
+        if not op.exists(new_go_mf):
+            shutil.copy2(old_go_mf, new_go_mf)
+        # TODO: parse GO_MF
+
+    go_bp = 'GO_BP.dat'
+    old_go_bp = op.join(model1_coach_folder, go_bp)
+    new_go_bp = op.join(dest_coach_dir, go_bp)
+    if op.exists(old_go_bp):
+        if not op.exists(new_go_bp):
+            shutil.copy2(old_go_bp, new_go_bp)
+        # TODO: parse GO_BP
+
+    go_cc = 'GO_CC.dat'
+    old_go_cc = op.join(model1_coach_folder, go_cc)
+    new_go_cc = op.join(dest_coach_dir, go_cc)
+    if op.exists(old_go_cc):
+        if not op.exists(new_go_cc):
+            shutil.copy2(old_go_cc, new_go_cc)
+        # TODO: parse GO_CC
+
+    return hom_dict
 
 def parse_init_dat(infile):
     """Parse the main init.dat file which contains the modeling results
@@ -7,13 +126,81 @@ def parse_init_dat(infile):
     "120 easy  40   8"
     # TODO: what is this?
 
+    The other lines look like this:
+    "     161   11.051   1  1guqA MUSTER"
+    and getting the first 10 gives you the top 10 templates used in modeling
+
     Args:
         infile:
 
     Returns:
 
     """
-    return
+    init_dict = {}
+
+    with open(infile, 'r') as f:
+        # Get first 2 lines of file
+        head = [next(f).strip() for x in range(2)]
+
+    summary = head[0].split()
+    difficulty = summary[1]
+
+    top_template_info = head[1].split()
+    top_template_pdbchain = top_template_info[3]
+    top_template_pdb = top_template_pdbchain[:4]
+    top_template_chain = top_template_pdbchain[4:]
+
+    init_dict['difficulty'] = difficulty
+    init_dict['top_template_pdb'] = top_template_pdb
+    init_dict['top_template_chain'] = top_template_chain
+
+    return init_dict
+
+def parse_seq_dat(infile):
+    """Parse the secondary structure predictions in seq.dat
+
+    Args:
+        infile: Path to seq.dat
+
+    Returns:
+        dict: Dictionary of residue number to secondary structure
+
+    """
+    pass
+
+def parse_cscore(infile):
+    """Parse the cscore file to return a dictionary of scores.
+
+    Args:
+        infile: Path to cscore
+
+    Returns:
+        dict: Dictionary of scores
+
+    """
+    cscore_dict = {}
+    with open(infile, 'r') as f:
+        lines = f.readlines()
+        for ll in lines:
+            # Look for the first line that starts with model1
+            if ll.lower().startswith('model1'):
+                l = ll.split()
+
+                cscore = l[1]
+                tmscore_full = l[2].split('+-')
+                tmscore = tmscore_full[0]
+                tmscore_err = tmscore_full[1]
+                rmsd_full = l[3].split('+-')
+                rmsd = rmsd_full[0]
+                rmsd_err = rmsd_full[1]
+
+                cscore_dict['c_score'] = float(cscore)
+                cscore_dict['tm_score'] = float(tmscore)
+                cscore_dict['tm_score_err'] = float(tmscore_err)
+                cscore_dict['rmsd'] = float(rmsd)
+                cscore_dict['rmsd_err'] = float(rmsd_err)
+
+    return cscore_dict
 
 def parse_bsites_inf(infile):
     """Parse the Bsites.inf output file of COACH and returns a dataframe
@@ -23,7 +210,7 @@ def parse_bsites_inf(infile):
     For each site (cluster), there are three lines:
     Line 1: site number, c-score of coach prediction, cluster size
     Line 2: algorithm, PDB ID, ligand ID, center of binding site (cartesian coordinates),
-        c-score of the algorithm’kegg prediction, binding residues from single template
+        c-score of the algorithm's prediction, binding residues from single template
     Line 3: Statistics of ligands in the cluster
 
     Args:
