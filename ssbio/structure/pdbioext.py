26cfc2eb2b19551729e2b0e5dfea7973bbc1b9bf
Nathan <nmih@ucsd.edu>
2016-11-21 22:06:04 -0800

    Bug fixes

--- ssbio/structure/pdbioext.py
+++ ssbio/structure/pdbioext.py
@@ -41,7 +41,7 @@ class PDBIOExt(PDBIO):
         self.set_structure(structure)
         self.first_model = structure[0]
 
-    def write_pdb(self, custom_name='', out_suffix='_new', outdir=None, custom_selection=None):
+    def write_pdb(self, custom_name='', out_suffix='_new', out_dir=None, custom_selection=None):
         """Write a new PDB file for the Structure's FIRST MODEL.
 
         Set custom_selection to a PDB.Select class for custom SMCRA selections.
@@ -57,7 +57,7 @@ class PDBIOExt(PDBIO):
         """
 
         # Prepare the output file path
-        outfile = ssbio.utils.outfile_name_maker(inname=self.structure_file, outfile=custom_name, append_to_name=out_suffix, outdir=outdir, outext='.pdb')
+        outfile = ssbio.utils.outfile_name_maker(inname=self.structure_file, outfile=custom_name, append_to_name=out_suffix, outdir=out_dir, outext='.pdb')
         self.save(outfile, custom_selection)
 
         return outfile
