689b5f8d80040f5ab2838ae2e1457c4de7e4a3d1
Nathan <nmih@ucsd.edu>
2016-12-09 18:56:12 -0800

    Updates

--- ssbio/databases/bigg.py
+++ ssbio/databases/bigg.py
@@ -8,7 +8,7 @@ from bioservices.uniprot import UniProt
 bs_unip = UniProt()
 
 
-def get_pdbs_for_gene(bigg_model, bigg_gene, cache=False, cache_dir=tempfile.gettempdir()):
+def get_pdbs_for_gene(bigg_model, bigg_gene, cache_dir=tempfile.gettempdir()):
     """Attempt to get a rank-ordered list of available PDB structures for a BiGG Model and its gene.
 
     Args:
@@ -20,13 +20,16 @@ def get_pdbs_for_gene(bigg_model, bigg_gene, cache=False, cache_dir=tempfile.get
 
     """
     my_structures = []
-    gene_raw = requests.get('http://bigg.ucsd.edu/api/v2/models/{}/genes/{}'.format(bigg_model, bigg_gene))
-    gene = gene_raw.json()
 
-    if cache:
-        outfile = op.join(cache_dir, '{}.json'.format(bigg_gene))
+    outfile = op.join(cache_dir, '{}_genes.json'.format(bigg_model))
+    if not op.exists(outfile):
+        gene_raw = requests.get('http://bigg.ucsd.edu/api/v2/models/{}/genes/{}'.format(bigg_model, bigg_gene))
+        gene = gene_raw.json()
         with open(outfile, 'w') as f:
             json.dump(gene, f)
+    else:
+        with open(outfile, 'r') as f:
+            gene = json.load(f)
 
     uniprots = []
     if 'database_links' in gene:
@@ -35,17 +38,13 @@ def get_pdbs_for_gene(bigg_model, bigg_gene, cache=False, cache_dir=tempfile.get
         elif 'NCBI GI' in gene['database_links']:
             uniprots = []
             gis = [x['id'] for x in gene['database_links']['NCBI GI']]
-            for gi in gis:
-                gi_uniprots = bs_unip.mapping(fr='P_GI', to='ACC', query=gis).values()
+            gi_uniprots = bs_unip.mapping(fr='P_GI', to='ACC', query=gis).values()
             uniprots.extend(gi_uniprots)
             uniprots = ssbio.utils.flatlist_dropdup(uniprots)
 
     if uniprots:
         for u in uniprots:
-            if cache:
-                get_best_structure = ssbio.databases.pdb.best_structures(uniprot_id=u, outdir=cache_dir)
-            else:
-                get_best_structure = ssbio.databases.pdb.best_structures(uniprot_id=u)
+            get_best_structure = ssbio.databases.pdb.best_structures(uniprot_id=u, outdir=cache_dir)
             if get_best_structure:
                 for best_structure in get_best_structure:
                     my_structures.append((best_structure['pdb_id'], best_structure['chain_id']))
