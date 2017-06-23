17728941028dfbf2cb9b627cf615b0c865e3e022
nmih <nmih@ucsd.edu>
2017-06-23 15:25:00 -0700

    Cleanup old files

--- install-irkernel.R
+++
@@ -1,6 +0,0 @@
-# see https://github.com/ctb/2016-mybinder-irkernel
-install.packages(c('rzmq','repr','IRkernel','IRdisplay'),
-                 repos = c('http://irkernel.github.io/',
-                           'http://cran.us.r-project.org'))
-
-IRkernel::installspec()
