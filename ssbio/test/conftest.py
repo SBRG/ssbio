5f4106d666268dfb8a4a29fd1ae92e83ab4ce92f
nmih <nmih@ucsd.edu>
2018-02-24 18:29:48 -0800

    Add more unit tests

--- ssbio/test/conftest.py
+++ ssbio/test/conftest.py
@@ -4,52 +4,67 @@ import tempfile
 
 
 @pytest.fixture(scope='module')
+def test_files_tempdir(test_files):
+    # /tmp/
+    return tempfile.gettempdir()
+
+
+@pytest.fixture(scope='module')
 def test_files():
     return 'test_files'
 
 
 @pytest.fixture(scope='module')
 def test_files_sequences(test_files):
+    """Sequence files"""
+    # ssbio/test/test_files/sequences
     return op.join(test_files, 'sequences')
 
 
 @pytest.fixture(scope='module')
 def test_files_structures(test_files):
+    """Structure files"""
+    # ssbio/test/test_files/structures
     return op.join(test_files, 'structures')
 
 
 @pytest.fixture(scope='module')
 def test_files_models(test_files):
+    """SBML model files"""
+    # ssbio/test/test_files/models
     return op.join(test_files, 'models')
 
 
 @pytest.fixture(scope='module')
 def test_files_outputs(test_files):
+    """ssbio local output directory - likely to use tempdir in most cases"""
+    # ssbio/test/test_files/out
     return op.join(test_files, 'out')
 
 
 @pytest.fixture(scope='module')
-def test_files_tempdir(test_files):
-    return tempfile.gettempdir()
-
-
-@pytest.fixture(scope='module')
-def test_gem_small_sbml(test_files_models):
-    return op.join(test_files_models, 'Ec_core_flux1.xml')
+def test_gem_small_json(test_files_models):
+    """Mini JSON GEM for testing (E. coli)"""
+    # ssbio/test/test_files/models/mini.json
+    return op.join(test_files_models, 'mini.json')
 
 
 @pytest.fixture(scope='module')
 def test_gem_large_json(test_files_models):
+    """Large JSON GEM for testing (MTB)"""
+    # ssbio/test/test_files/models/iNJ661.json
     return op.join(test_files_models, 'iNJ661.json')
 
 
 @pytest.fixture(scope='module')
 def list_of_genes_ecoli():
+    """List of genes for testing (E. coli)"""
     return ['b0720','b1241','b1761','b0003']
 
 
 @pytest.fixture(scope='module')
 def dict_of_genes_seqs_ecoli():
+    """Dictionary of genes and sequence strings for testing (E. coli)"""
     return {'b0870': 'MIDLRSDTVTRPSRAMLEAMMAAPVGDDVYGDDPTVNALQDYAAELSGKEAAIFLPTGTQANLVALLSHCERGEEYIVGQAAHNYLFEAGGAAVLGSIQPQPIDAAADGTLPLDKVAMKIKPDDIHFARTKLLSLENTHNGKVLPREYLKEAWEFTRERNLALHVDGARIFNAVVAYGCELKEITQYCDSFTICLSKGLGTPVGSLLVGNRDYIKRAIRWRKMTGGGMRQSGILAAAGIYALKNNVARLQEDHDNAAWMAEQLREAGADVMRQDTNMLFVRVGEENAAALGEYMKARNVLINASPIVRLVTHLDVSREQLAEVAAHWRAFLAR',
             'b3041': 'MNQTLLSSFGTPFERVENALAALREGRGVMVLDDEDRENEGDMIFPAETMTVEQMALTIRHGSGIVCLCITEDRRKQLDLPMMVENNTSAYGTGFTVTIEAAEGVTTGVSAADRITTVRAAIADGAKPSDLNRPGHVFPLRAQAGGVLTRGGHTEATIDLMTLAGFKPAGVLCELTNDDGTMARAPECIEFANKHNMALVTIEDLVAYRQAHERKAS'}
 
